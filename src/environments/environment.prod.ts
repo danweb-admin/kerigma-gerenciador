e2563e3f4c361bbee45101d3d5aa4aba08446474
William Nascimento <william.nascimento2010@gmail.com>
2026-01-21 14:18:31 -0300

    url backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    URL_API: 'http://161.35.255.131:31354',
+    URL_API: 'https://backend2.rcc-londrina.online',
+    // URL_API: 'http://161.35.255.131:31354',
 };
