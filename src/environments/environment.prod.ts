cba2f2c187fb3ee1b5a8728d509af08ef83d96fa
danweb-admin <143641802+danweb-admin@users.noreply.github.com>
2026-03-29 18:47:49 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    URL_API: 'https://backend.rcc-londrina.online',
+    URL_API: 'https://backend.kerigma-eventos.online',
     // URL_API: 'http://161.35.255.131:31354',
 };
